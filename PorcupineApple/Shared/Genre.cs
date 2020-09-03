08810b95c7b96b286fc3c28cb8e6c5e3d02e3934
fbatisteti <fbatisteti@hotmail.com>
2020-09-03 20:50:11 -0300

    Rough draft of a user create

--- PorcupineApple/Shared/Genre.cs
+++ PorcupineApple/Shared/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PorcupineApple.Shared
@@ -7,6 +8,9 @@ namespace PorcupineApple.Shared
     public class Genre
     {
         public int Id { get; set; }
+
+        //[Required (ErrorMessage = "message")]
+        [Required]
         public string Name { get; set; }
     }
 }
---
+++ PorcupineApple/Shared/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PorcupineApple.Shared
+{
+    public class User
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        // requires e-mail to be "SOMETHING + @SOMETHING + .SOMETHING + SOMETHING"
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please inform a valid e-mail")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage = "Password must contain at least 6 characters")]
+        // requires a minimum of 6 characters in password, up to a maximum of an absurdly high number
+        [StringLength(255, MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
